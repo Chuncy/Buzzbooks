657606a2c0bd9e2e2a218c3a170660b352f8133a
Chuncy <row3711@icloud.com>
2018-09-19 11:19:07 +0900

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :name, {presence: true}
+  validates :email, {presence: true, uniqueness: true}
+  
+  def posts
+    return Post.where(user_id: self.id)
+  end
+
+
+end
